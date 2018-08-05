6c5e116024dfacbe61c3ab644c5a19ab8cff4071
Junian Triajianto <author@junian.net>
2018-08-05 14:22:19 +0700

    Add blcokadblock widget

--- assets/js/fokus.js
+++ assets/js/fokus.js
@@ -9,3 +9,39 @@ if (typeof jQuery === 'undefined') {
         throw new Error('Bootstrap\'s JavaScript requires jQuery version 1.9.1 or higher, but lower than version 4')
     }
 }(jQuery);
+
+
+$(function() {
+
+    function adBlockDetected() {
+        console.log("adblock detected");
+        $('noscript').before($('noscript').text());
+        $('#blockadblock').removeClass('hidden');
+    }
+
+    function adBlockNotDetected() {
+        console.log("adblock not detected");
+    }
+    
+    if(typeof blockAdBlock === 'undefined') {
+        adBlockDetected();
+    } else {
+        blockAdBlock.setOption({ debug: false });
+        blockAdBlock.onDetected(adBlockDetected).onNotDetected(adBlockNotDetected);
+    }
+    
+    function checkAgain() {
+        $('#block-adb-enabled').hide();
+        $('#block-adb-not-enabled').hide();
+        // setTimeout 300ms for the recheck is visible when you click on the button
+        setTimeout(function() {
+            if(typeof blockAdBlock === 'undefined') {
+                adBlockDetected();
+            } else {
+                blockAdBlock.onDetected(adBlockDetected).onNotDetected(adBlockNotDetected);
+                blockAdBlock.check();
+            }
+        }, 300);
+    }
+
+ });
