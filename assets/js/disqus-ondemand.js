8d63da288fbab1126d06ce13d9368358c69fb46d
Junian Triajianto <author@junian.net>
2018-08-09 15:15:19 +0700

    Fix disqus count

--- assets/js/disqus-ondemand.js
+++ assets/js/disqus-ondemand.js
@@ -3,24 +3,31 @@ function loadDisqusOnDemand() {
     // Replace this value with *your* username.
     // This is defined at fokus.js
     // var disqus_shortname = '{{ .Site.disqusShortname }}'; 
-
-    var dsqCount = document.createElement('script');
     
-    dsqCount.src = "https://" + disqus_shortname + ".disqus.com/count.js";
-    dsqCount.id = "dsq-count-scr";
-    dsqCount.async = true;
-
-    document.body.appendChild(dsqCount);
+    var disqus_config = function () {
+        this.page.url = pageUrl;
+    };
 
     $('#btn_load_disqus_thread').on('click', function() {
         // ajax request to load the disqus javascript
-        $.ajax({
-            type: "GET",
-            url: "https://" + disqus_shortname + ".disqus.com/embed.js",
-            dataType: "script",
-            cache: true
-        });
+        (function() {
+            var disqus = document.createElement('script'); 
+            disqus.type = 'text/javascript'; 
+            disqus.async = true;
+            disqus.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(disqus);
+        })();
+
         // hide the button once comments load
         $(this).fadeOut();
     });
+
+    (function() {
+        var dsqCount = document.createElement('script');
+        dsqCount.type = 'text/javascript'; 
+        dsqCount.async = true;
+        dsqCount.src = "//" + disqus_shortname + ".disqus.com/count.js";
+        dsqCount.id = "dsq-count-scr";
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsqCount);
+    })();
 }
