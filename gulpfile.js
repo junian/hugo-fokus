db5fb1a244e69a8a42bb1fe42765dd92d5f18683
Junian Triajianto <author@junian.net>
2018-08-14 10:14:31 +0700

    Use media query packer later

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
+var mqpacker = require("css-mqpacker");
 
 gulp.task('css', function() {
     var processors = [
-        autoprefixer
+        autoprefixer,
+        mqpacker
     ];
     gulp.src('./assets/scss/**/*.scss')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
