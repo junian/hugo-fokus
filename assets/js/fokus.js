099b6fa829afc90e5a7182d1d96ecece9120cd1c
Junian Triajianto <author@junian.net>
2018-08-17 05:54:43 +0700

    Better clipboardjs support

--- assets/js/fokus.js
+++ assets/js/fokus.js
@@ -19,6 +19,13 @@
         el.insertAdjacentHTML('beforebegin', htmlString);
     }
 
+    function addClass(el,className) {
+        if (el.classList)
+            el.classList.add(className);
+        else
+            el.className += ' ' + className;
+    }
+
     function removeClass(el,className) {
         if (el.classList)
             el.classList.remove(className);
@@ -213,21 +220,46 @@
     }
 
     function runClipboardjs() {
+        if (!ClipboardJS.isSupported()) {
+            console.log("Clipboard js not supported");
+            var blocks = document.querySelectorAll("figure.highlight > .btn-clipboard");
+            Array.prototype.forEach.call(blocks, function (block, index) {
+                remove(block);
+            });
+            return;
+        }
+    
         var clipboard = new ClipboardJS('.btn-clipboard', {
             target: function(trigger) {
                 return trigger.nextElementSibling;
             }
         });
-
-        clipboard.on('success', function(e) {
+    
+        clipboard.on('success', function (e) {
             console.info('Action:', e.action);
-            console.info('Text:', e.text);
+            // console.info('Text:', e.text);
             console.info('Trigger:', e.trigger);
-
+    
             e.clearSelection();
+    
+            var message = e.trigger.querySelector(".msg-clipboard");
+    
+            if (!message) return;
+    
+            if (message.getAttribute('data-cbtimer')) clearTimeout(parseInt(message.getAttribute('data-cbtimer'), 10));
+    
+            /* Set message indicating success and show it */
+            addClass(message, "msg-clipboard--active");
+            message.innerHTML = "Copied to clipboard";
+    
+            /* Hide message after two seconds */
+            message.setAttribute('data-cbtimer', setTimeout(function () {
+                removeClass(message, ("msg-clipboard--active"));
+                message.setAttribute('data-cbtimer', "");
+            }, 2000).toString());
         });
-
-        clipboard.on('error', function(e) {
+    
+        clipboard.on('error', function (e) {
             console.error('Action:', e.action);
             console.error('Trigger:', e.trigger);
         });
