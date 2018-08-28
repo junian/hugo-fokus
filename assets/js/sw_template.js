162c98a8dbf0787fc52a74e994c4050edaeed530
Junian Triajianto <author@junian.net>
2018-08-28 15:14:55 +0700

    Fix sw_template

--- assets/js/sw_template.js
+++ assets/js/sw_template.js
@@ -3,7 +3,7 @@
  * Copyright 2018 Junian Triajianto
  * Licensed under MIT (https://github.com/junian/fokus/blob/master/LICENSE)
  */
- /*
+/*
 This is a modified version of Ethan Marcotte's service worker (https://ethanmarcotte.com/theworkerofservices.js),
 which is in turn a modified version of Jeremy Keith's service worker (https://adactio.com/serviceworker.js),
 with a few additional edits borrowed from Filament Group's. (https://www.filamentgroup.com/sw.js)
@@ -11,13 +11,13 @@ with a few additional edits borrowed from Filament Group's. (https://www.filamen
 
 (function() {
   const version = 'v{{ .Scratch.Get "swversion" }}';
-  const cacheName = version + '::fokus:';
+  const cacheName = version + "::fokus:";
 
-  const staticCacheName = cacheName + 'static';
-  const pagesCacheName = cacheName + 'pages';
+  const staticCacheName = cacheName + "static";
+  const pagesCacheName = cacheName + "pages";
 
   const staticAssets = [
-    '/',
+    "/",
     '{{ .Scratch.Get "appcss" }}',
     '{{ .Scratch.Get "appjs" }}'
   ];
@@ -25,101 +25,101 @@ with a few additional edits borrowed from Filament Group's. (https://www.filamen
   function updateStaticCache() {
     // These items must be cached for the Service Worker to complete installation
     return caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(staticAssets.map(url => new Request(url, {credentials: 'include'})));
-    });
+      .then((cache) => {
+        return cache.addAll(staticAssets.map((url) => new Request(url, {
+          credentials: "include"
+        })));
+      });
   }
 
   function stashInCache(cacheName, request, response) {
     caches.open(cacheName)
-    .then(cache => cache.put(request, response));
+      .then((cache) => cache.put(request, response));
   }
 
   // Limit the number of items in a specified cache.
   function trimCache(cacheName, maxItems) {
     caches.open(cacheName)
-    .then(cache => {
-      cache.keys()
-      .then(keys => {
-        if (keys.length > maxItems) {
-          cache.delete(keys[ 0 ])
-          .then(trimCache(cacheName, maxItems));
-        }
+      .then((cache) => {
+        cache.keys()
+          .then((keys) => {
+            if (keys.length > maxItems) {
+              cache.delete(keys[0])
+                .then(trimCache(cacheName, maxItems));
+            }
+          });
       });
-    });
   }
 
   // Remove caches whose name is no longer valid
   function clearOldCaches() {
     return caches.keys()
-    .then(keys => {
-      return Promise.all(keys
-        .filter(key => key.indexOf(version) !== 0)
-        .map(key => caches.delete(key))
-       );
-    });
+      .then((keys) => {
+        return Promise.all(keys
+          .filter((key) => key.indexOf(version) !== 0)
+          .map((key) => caches.delete(key))
+        );
+      });
   }
 
   // Events!
-  self.addEventListener('message', event => {
-    if (event.data.command === 'trimCaches') {
+  self.addEventListener("message", (event) => {
+    if (event.data.command === "trimCaches") {
       trimCache(pagesCacheName, 35);
     }
   });
 
-  self.addEventListener('install', event => {
+  self.addEventListener("install", (event) => {
     event.waitUntil(updateStaticCache()
       .then(() => self.skipWaiting())
-     );
+    );
   });
 
-  self.addEventListener('activate', event => {
+  self.addEventListener("activate", (event) => {
     event.waitUntil(clearOldCaches()
       .then(() => self.clients.claim())
-     );
+    );
   });
 
-  self.addEventListener('message', event => {
-    if (event.data.command === 'trimCaches') {
+  self.addEventListener("message", (event) => {
+    if (event.data.command === "trimCaches") {
       trimCache(pagesCacheName, 35);
     }
   });
 
-  self.addEventListener('fetch', event => {
+  self.addEventListener("fetch", (event) => {
     const request = event.request;
     const url = new URL(request.url);
 
-    if (url.href.indexOf('https://www.hartwell-insurance.com') !== 0) {
+    if (url.href.indexOf("{{ .Site.BaseURL }}") !== 0) {
       return;
     }
 
     // Ignore non-GET requests
-    if (request.method !== 'GET') {
+    if (request.method !== "GET") {
       return;
     }
 
     // Ignore query-stringÃ¢â‚¬â„¢d requests
-    if (url.href.indexOf('?') !== -1) {
+    if (url.href.indexOf("?") !== -1) {
       return;
     }
 
     // Try the network first, fall back to the cache, finally the offline page (for HTML requests)
     event.respondWith(
-      fetch(request)
-      .then(response => {
+      fetch(request).then((response) => {
         // NETWORK
         // Stash a copy of this page in the pages cache
         const copy = response.clone();
         stashInCache(staticCacheName, request, copy);
         return response;
-      })
-      .catch(() => {
+      }).catch(() => {
         // CACHE or FALLBACK
-        if (request.headers.get('Accept').indexOf('text/html') !== -1) {
+        if (request.headers.get("Accept").indexOf("text/html") !== -1) {
           return caches.match(request)
-            .then(response => response || caches.match('/offline/'));
+            .then((response) => response || caches.match("/offline/"));
         } else {
-          return caches.match(request).then(response => response);
+          return caches.match(request).then((response) => response);
         }
       })
     );
