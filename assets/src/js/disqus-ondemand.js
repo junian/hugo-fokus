b1f8f8e70486b147c5fed6ea064e04f23a2f5239
Junian Triajianto <author@junian.net>
2018-08-28 08:29:06 +0700

    Simplify disqus on demand

--- assets/src/js/disqus-ondemand.js
+++ assets/src/js/disqus-ondemand.js
@@ -1,26 +1,29 @@
 import {config} from "./config";
 
+const d = document;
+const n = config.disqus_shortname;
+
 function loadDisqusComments() {
-  if (!config.disqus_shortname)
+  if (!n)
     return;
   (() => { // DON'T EDIT BELOW THIS LINE
-    const d = document, s = d.createElement("script");
-    s.src = "https://" + config.disqus_shortname + ".disqus.com/embed.js";
+    const s = d.createElement("script");
+    s.src = "https://" + n + ".disqus.com/embed.js";
     s.setAttribute("data-timestamp", +new Date());
     (d.head || d.body).appendChild(s);
   })();
 }
 
 function loadDisqusCommentCount() {
-  if (!config.disqus_shortname)
+  if (!n)
     return;
   (() => {
-    const d = document, s = d.createElement("script");
+    const s = d.createElement("script");
     s.type = "text/javascript";
     s.async = true;
-    s.src = "https://" + config.disqus_shortname + ".disqus.com/count.js";
+    s.src = "https://" + n + ".disqus.com/count.js";
     s.id = "dsq-count-scr";
-    (document.getElementsByTagName("body")[0] || document.getElementsByTagName("head")[0]).appendChild(s);
+    (d.body || d.head).appendChild(s);
   })();
 }
 
