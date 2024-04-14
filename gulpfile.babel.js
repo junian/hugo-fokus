e6328f8beb140a3dd434bf226d22aa33a43d499b
Junian Triajianto <author@junian.dev>
2024-04-14 22:37:42 +0700

    Fix gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,68 +1,56 @@
-import gulp from "gulp";
-import sass from "gulp-sass";
+import { src, dest, pipe, series, parallel } from "gulp";
 import postcss from "gulp-postcss";
 import autoprefixer from "autoprefixer";
 import mqpacker from "css-mqpacker";
-import removeimportant from "@buddye/postcss-remove-important";
 import concat from "gulp-concat";
 import log from "fancy-log";
 import pluginError from "plugin-error";
 import webpack from "webpack";
-import webpackConfig from "./webpack.conf";
+import webpackConfig from "./webpack.config";
 import prettify from "gulp-jsbeautifier";
-import uncss from "gulp-uncss";
 
-gulp.task("bootstrap-custom-css", () => {
+const sass = require('gulp-sass')(require('sass'));
+
+function bootstrapCustomCss() {
   var processors = [
     autoprefixer,
     mqpacker
   ];
-  gulp.src("./assets/src/scss/bootstrap-custom.scss")
+  return src("./assets/src/scss/bootstrap-custom.scss")
     .pipe(sass({
       outputStyle: "expanded"
     }).on("error", sass.logError))
     .pipe(postcss(processors))
-    /*
-    .pipe(uncss({
-      html: [
-        "http://localhost:1313/page/2/",
-        "http://localhost:1313/privacy-policy/",
-        "http://localhost:1313/about/",
-        "http://localhost:1313/archives/",
-        "http://localhost:1313/xamarin-forms-video-background/"
-      ]
-    }))
-    */
-    .pipe(gulp.dest("./assets/css/"));
-});
+    .pipe(dest("./assets/css/"));
+}
 
-gulp.task("fokus-css", () => {
+function fokusCss(cb) {
   var processors = [
     autoprefixer,
     mqpacker
   ];
 
-  gulp.src(["./assets/src/scss/fokus.scss", "./assets/src/scss/fokus-print.scss"])
+  return src(["./assets/src/scss/fokus.scss", "./assets/src/scss/fokus-print.scss"])
     .pipe(sass({
       outputStyle: "expanded"
     }).on("error", sass.logError))
     .pipe(postcss(processors))
-    .pipe(gulp.dest("./assets/css/"));
-});
+    .pipe(dest("./assets/css/"));
+}
 
-gulp.task("css", ["bootstrap-custom-css", "fokus-css"], () => {
+function combineCss(cb) {
   var processors = [
     autoprefixer,
     mqpacker
   ];
-  return gulp.src(["./assets/css/bootstrap-custom.css", "./assets/css/fokus.css"])
+  return src(["./assets/css/bootstrap-custom.css", "./assets/css/fokus.css"])
     .pipe(concat("fokus-full.css"))
     .pipe(postcss(processors))
-    .pipe(gulp.dest("./assets/css/"));
-});
+    .pipe(dest("./assets/css/"));
+}
 
 // Compile Javascript
-gulp.task("packjs", (cb) => {
+function packjs(cb) {
   const myConfig = Object.assign({}, webpackConfig);
   webpack(myConfig, (err, stats) => {
     if (err) throw new pluginError("webpack", err);
@@ -72,17 +60,20 @@ gulp.task("packjs", (cb) => {
     })}`);
     cb();
   });
-});
+}
 
-gulp.task("js", ["packjs"], () => {
-  gulp.src("./assets/js/fokus_template.js")
+function prettifyjs(cb) {
+  src("./assets/js/fokus_template.js")
     .pipe(prettify({
       indent_size: 4,
       js: {
         indent_size: 2
       }
     }))
-    .pipe(gulp.dest("./assets/js"));
-});
+    .pipe(dest("./assets/js"));
+  cb();
+}
 
-gulp.task("default", ["css", "js"]);
+exports.jsbuild = series(packjs, prettifyjs);
+exports.cssbuild = series(bootstrapCustomCss, fokusCss, combineCss);
+exports.default = series(exports.cssbuild, exports.jsbuild);
